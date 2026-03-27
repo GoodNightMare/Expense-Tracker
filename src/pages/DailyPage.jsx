5d8d80786ffbd3da20d897fe9cf469cd884174fe
GoodNightMare <nonthee.night@gmail.com>
2026-03-28 01:55:55 +0700

    add color

--- src/pages/DailyPage.jsx
+++ src/pages/DailyPage.jsx
@@ -439,11 +439,16 @@ function DailyPage() {
                 className={`day-cell ${isWeekend ? "weekend" : ""} ${isHighlighted ? "highlighted" : ""}`}
               >
                 {specialExpenses > 100 && (
-                  <span 
-                    className="star-indicator" 
-                    style={{ color: specialExpenses > 200 ? 'red' : 'orange' }}
-                  >
-                    ⭐
+                  <span className="star-indicator">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill={specialExpenses > 200 ? 'red' : 'orange'}
+                      width="1em"
+                      height="1em"
+                    >
+                      <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+                    </svg>
                   </span>
                 )}
                 <span className="day-num">
